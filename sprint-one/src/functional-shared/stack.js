c464a4ec7875685127b5f3cf218ab288a2f4b340
willsimmons <willsimmons@gmail.com>
2016-09-14 15:05:08 -0700

    Pass first test for stack (functional-shared pattern)

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = stackMethods;
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  storage: {},
+  count: 0,
+  push: function(value) {
+
+  },
+  pop: function() {
+
+  },
+  size: function() {
+    return this.count;
+  }
+};
 
 
