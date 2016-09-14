fe621f5cfba5c7db367a023aeb4143104c027105
willsimmons <willsimmons@gmail.com>
2016-09-14 15:21:42 -0700

    Pass first test for queue (functional-shared)

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  instance.storage = {};
+  instance.front = 0;
+  instance.back = 0;
+  instance.enqueue = queueMethods.enqueue;
+  instance.dequeue = queueMethods.dequeue;
+  instance.size = queueMethods.size;
+  return instance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
 
+};
+
+queueMethods.dequeue = function() {
+
+};
+
+queueMethods.size = function() {
+  return this.back - this.front;
+};
