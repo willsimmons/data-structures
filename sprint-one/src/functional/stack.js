5eea523804ea9ff7d8c256c40fc0b0c5b6d92d71
William Simmons <willsimmons@gmail.com>
2016-09-14 12:52:53 -0700

    passing most stack tests...

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,28 @@
 var Stack = function() {
   var someInstance = {};
+  var items = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage["items"] = value;
+    items++;
   };
 
   someInstance.pop = function() {
+    var result = storage["items"];
+    delete storage["items"];
+    items--;
+    return result;
   };
 
   someInstance.size = function() {
+    if (items < 0) {
+      items = 0;
+    }
+    return items;
   };
 
   return someInstance;
