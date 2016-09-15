7e8ed5583e58089bf0f489ca3786baa5b52a2ebe
willsimmons <willsimmons@gmail.com>
2016-09-14 20:08:59 -0700

    Pass all tests for linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,32 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = new Node(value);
+    if (this.tail !== null) {
+      this.tail.next = node;
+    }
+    this.tail = node;
+    if (this.head === null) {
+      this.head = node;
+    }
   };
 
   list.removeHead = function() {
+    var result = this.head;
+    this.head = this.head.next;
+    return result.value;
   };
 
   list.contains = function(target) {
+    var contains = false;
+    var currNode = this.head;
+    while (currNode !== null) {
+      if (currNode.value === target) {
+        contains = true;
+      }
+      currNode = currNode.next;
+    }
+    return contains;
   };
 
   return list;
