135f5d764f12fb0290500eb21823692ddcf7122f
willsimmons <willsimmons@gmail.com>
2016-09-15 09:45:05 -0700

    Refactor set to use HashTable

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,27 +1,33 @@
 var Set = function() {
-  var set = Object.create(setPrototype);
-  set._storage = [];
-  return set;
+  // var set = Object.create(setPrototype);
+  // set._storage = [];
+  // return set;
+  var set = new HashTable();
+  return _.extend(set, setPrototype);
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  if (this._storage.indexOf(item) === -1) {
-    this._storage.push(item);
-  }
+  // if (this._storage.indexOf(item) === -1) {
+  //   this._storage.push(item);
+  // }
+  this.insert(item, item);
 };
 
 setPrototype.contains = function(item) {
-  return this._storage.indexOf(item) !== -1;
+  // return this._storage.indexOf(item) !== -1;
+  return this.retrieve(item) !== undefined;
 };
 
-setPrototype.remove = function(item) {
-  var index = this._storage.indexOf(item);
-  if (index !== -1) {
-    this._storage.splice(index, 1);
-  }
-};
+// uses the remove method from HashTable
+
+// setPrototype.remove = function(item) {
+//   var index = this._storage.indexOf(item);
+//   if (index !== -1) {
+//     this._storage.splice(index, 1);
+//   }
+// };
 
 /*
  * Complexity: What is the time complexity of the above functions?
