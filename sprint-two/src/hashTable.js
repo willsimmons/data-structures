b4bc425c9cad246305b9bf2dec5e9dadd3b28649
willsimmons <willsimmons@gmail.com>
2016-09-15 09:28:06 -0700

    Pass all tests for hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,32 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._collisions = {};
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index) !== undefined) {
+    while(this._storage.get(index) !== undefined) {
+      index = index + 1;
+    }
+    this._collisions[k] = index;
+  }
+  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this._collisions[k] !== undefined ? 
+                this._collisions[k] :
+                getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this._collisions[k] !== undefined ? 
+                this._collisions[k] :
+                getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
