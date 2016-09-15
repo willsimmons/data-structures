6c823a7f651089c738b8cfca973c09ff4661543a
willsimmons <willsimmons@gmail.com>
2016-09-15 10:07:47 -0700

    Pass all tests for bST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,58 @@
 var BinarySearchTree = function(value) {
+  var bST = Object.create(bSTmethods);
+  bST.value = value;
+  bST.left = null;
+  bST.right = null;
+  return bST;
 };
 
+var bSTmethods = {};
 
+bSTmethods.insert = function(value) {
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (this.right === null) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+bSTmethods.contains = function(value) {
+  if (this.value === value) {
+    return true;
+  } else {
+    if (value < this.value) {
+      if (this.left === null) {
+        return false;
+      } else {
+        return this.left.contains(value);
+      }
+    } else {
+      if (this.right === null) {
+        return false;
+      } else {
+        return this.right.contains(value);
+      }
+    }
+  }
+};
+
+bSTmethods.depthFirstLog = function(cb) {
+  cb(this.value);
+  if (this.left !== null) {
+    this.left.depthFirstLog(cb);
+  }
+  if (this.right !== null) {
+    this.right.depthFirstLog(cb);
+  }
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
