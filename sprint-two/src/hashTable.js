c421391d9d2f946f1c1d079a9c7cf804730a013d
willsimmons <willsimmons@gmail.com>
2016-09-16 10:46:15 -0700

    Amend hash table to store arrays in storage

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,27 +8,50 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if (this._storage.get(index) !== undefined) {
-    while(this._storage.get(index) !== undefined) {
-      index = index + 1;
-    }
-    this._collisions[k] = index;
+  // if (this._storage.get(index) !== undefined) {
+  //   while(this._storage.get(index) !== undefined) {
+  //     index = index + 1;
+  //   }
+  //   this._collisions[k] = index;
+  // }
+  var block = [k, v];
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, [block]);
+  } else {
+    var blocks = this._storage.get(index);
+    blocks.push(block);
+    this._storage.set(index, blocks);
   }
-  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = this._collisions[k] !== undefined ? 
-                this._collisions[k] :
-                getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  // var index = this._collisions[k] !== undefined ? 
+  //               this._collisions[k] :
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var blocks = this._storage.get(index);
+  var answer;
+  blocks.forEach(function(block) {
+    if (block[0] === k) {
+      answer = block[1];
+    }
+  });
+  console.log(answer);
+  return answer;
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = this._collisions[k] !== undefined ? 
-                this._collisions[k] :
-                getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, undefined);
+  // var index = this._collisions[k] !== undefined ? 
+  //               this._collisions[k] :
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var blocks = this._storage.get(index);
+  // var blockSplicePosition;
+  blocks.forEach(function(block, ind) {
+    if (block[0] === k) {
+      // blockSplicePosition = ind;
+      blocks.splice(ind, 1);
+    }
+  });
+  // this._storage.set(index, blocks);
 };
 
 
